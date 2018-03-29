a8d040e39adb8aae2c8d3cea7f7eb1f5ca0e4781
cristi.alexandrescu <necrocris2000@yahoo.com>
2018-03-29 11:00:40 +0300

    Immutable
    more examples

--- Immutable/Program.cs
+++ Immutable/Program.cs
@@ -15,7 +15,17 @@ namespace test
             testString = testString + "a";
         }
 
+        static void testString(String testString)
+        {
+            testString = testString + "a";
+        }
+
         static void testInt(ref Int32 testNum)
+        {
+            testNum++;
+        }
+
+        static void testInt(Int32 testNum)
         {
             testNum++;
         }
@@ -34,15 +44,19 @@ namespace test
         static void Main(string[] args)
         {
             String nameString = "abc";
+            Int32 nameNum = 2;
             List<Int32> testList = new List<Int32>();
             Student std = new Student { Name = "ABC"};
+            testString(nameString);
             testString(ref nameString);
-            Int32 nameNum = 2;
             testInt(ref nameNum);
+            testInt(nameNum);
             AddToList(testList);
             ChangeStudent(std);
             Console.WriteLine(nameString);
             Console.WriteLine(nameNum);
+            Console.WriteLine(std.Name);
+            Console.WriteLine(testList[0]);
         }
     }
 }
