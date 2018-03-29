39f71b5a1e6cb6eee946e1d92239eb62dfa69ef0
cristi.alexandrescu <necrocris2000@yahoo.com>
2018-03-29 10:17:59 +0300

    logger
    added logger to log every event

---
+++ Student/Logger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Main
+{
+    public sealed class Logger
+    {
+        private static Logger instance = new Logger();
+
+        private Logger()
+        {
+        }
+
+        public static Logger Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public void LogInfo(TextWriter textWriter, String text)
+        {
+            textWriter.Write("Info  : {0}\n", text);
+        }
+
+        public void LogError(TextWriter textWriter, String text)
+        {
+            textWriter.Write("Error : {0}\n", text);
+        }
+
+        public void LogWarning(TextWriter textWriter, String text)
+        {
+            textWriter.Write("Warning : {0}\n", text);
+        }
+    }
+}
--- Student/Program.cs
+++ Student/Program.cs
@@ -7,8 +7,47 @@ namespace Main
 {
     class Program
     {
+        private static TextWriter GetOutputTextWriter()
+        {
+            var writer = new StreamWriter(File.OpenWrite("logger.txt"));
+            return writer;
+        }
+
         static void Main(string[] args)
         {
+            TextWriter textWriter = null;
+            try
+            {
+                Logger logger = Logger.Instance;
+
+                textWriter = GetOutputTextWriter();
+
+                logger.LogInfo(textWriter, "1 + 1 = 2");
+
+                logger.LogWarning(textWriter, "1 + 1 = 3");
+
+                logger.LogError(textWriter, "1 / 0 = ?");
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("WARNING: File is not available. Error Message: " + e.Message);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(String.Format("Output Exception ocurred: '{0}'", e.Message));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("General Exception ocurred. Error Message: " + e.Message);
+            }
+            finally
+            {
+                if (textWriter != null)
+                {
+                    textWriter.Close();
+                }
+            }
+            /*
             StudentModel student = new StudentModel("nume", "preume", "group", 1);
             List<StudentModel> students = new List<StudentModel>();
 
@@ -19,6 +58,7 @@ namespace Main
             MainView mainView = new MainView(controller);
 
             mainView.Execute();
+            */
         }
     }
 }
