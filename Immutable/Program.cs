427f47b07806f7625dc9d91709f674e97bcbccfe
cristi.alexandrescu <necrocris2000@yahoo.com>
2018-03-29 10:54:15 +0300

    Immutable
    pass by reference C# vs pass by value

---
+++ Immutable/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace test
+{
+    class Student
+    {
+        public string Name { get; set; }
+    }
+    class Program
+    {
+        
+        static void testString(ref String testString)
+        {
+            testString = testString + "a";
+        }
+
+        static void testInt(ref Int32 testNum)
+        {
+            testNum++;
+        }
+
+        static void AddToList(List<Int32> list)
+        {
+            list.Add(3);
+        }
+
+        static void ChangeStudent(Student std)
+        {
+            std.Name = "TEST";
+            std = new Student { Name="Antother TEST"};
+        }
+
+        static void Main(string[] args)
+        {
+            String nameString = "abc";
+            List<Int32> testList = new List<Int32>();
+            Student std = new Student { Name = "ABC"};
+            testString(ref nameString);
+            Int32 nameNum = 2;
+            testInt(ref nameNum);
+            AddToList(testList);
+            ChangeStudent(std);
+            Console.WriteLine(nameString);
+            Console.WriteLine(nameNum);
+        }
+    }
+}
